6384c2b1702dcf959402325fb126e47e0f77ac17
Asterios Katsifodimos <asteriosk@gmail.com>
2014-07-06 20:44:42 +0200

    Added scripts to visualize points etc.

---
+++ showPoints.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python plotPoints.py points result-points points
+evince points-plot.pdf 
---
+++ showResults.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python plotPoints.py result result-points results
+evince results-plot.pdf 
